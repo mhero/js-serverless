9f778c5ca64aea63c671a2c2a73a4fd19dfc9dd8
Marco <gdmarav374@gmail.com>
2021-08-03 18:21:20 +0200

    Move AIM into method

--- auth/authorize.js
+++ auth/authorize.js
@@ -12,6 +12,18 @@ const authorizeUser = (userScopes, methodArn) => {
     return hasValidScope;
 };
 
+const aimPolicy = (event, user) => {
+    // Return an IAM policy document for the current endpoint
+    const isAllowed = authorizeUser(user.scopes, event.methodArn);
+    const effect = isAllowed ? 'Allow' : 'Deny';
+    const userId = user.username;
+    const authorizerContext = {
+        user: JSON.stringify(user)
+    };
+
+    return utils.buildIAMPolicy(userId, effect, event.methodArn, authorizerContext);
+}
+
 module.exports.handler = (event, context, callback) => {
     const token = event.authorizationToken;
 
@@ -21,15 +33,7 @@ module.exports.handler = (event, context, callback) => {
 
         // Checks if the user's scopes allow her to call the current endpoint ARN
         const user = decoded.user;
-        const isAllowed = authorizeUser(user.scopes, event.methodArn);
-
-        // Return an IAM policy document for the current endpoint
-        const effect = isAllowed ? 'Allow' : 'Deny';
-        const userId = user.username;
-        const authorizerContext = {
-            user: JSON.stringify(user)
-        };
-        const policyDocument = utils.buildIAMPolicy(userId, effect, event.methodArn, authorizerContext);
+        const policyDocument = aimPolicy(event, user);
 
         console.log('Returning IAM policy document');
         callback(null, policyDocument);
